461a09ff51f198f6b8735187ed5acf66bbdd25ce
Enzo Catalan <enzocv96@gmail.com>
2016-11-07 14:20:08 -0500

    firts commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Enzocv on 23/01/2016.
+ */
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+var reload = browserSync.reload;
+
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('watch', function () {
+    gulp.watch("css/*.css", ['autoprefixer']);
+    gulp.watch('./*.html').on("change", reload);
+});
+
+gulp.task('autoprefixer', function () {
+    return gulp.src('css/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 5 versions','> 1%',
+                'ie 8',
+                'ie 9',
+                'ios 6',
+                'android 4'],
+            cascade: true
+        }))
+        .pipe(gulp.dest('css'))
+        .pipe(reload({
+            stream: true
+        }))
+});
+
+gulp.task('default', ['watch', 'browser-sync']);
---
+++ toggle-search/_search-toggle-panel.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Enzocv on 31/01/2016.
+ */
+var buttom1 = document.getElementById('click1');
+var element1 = document.getElementById('added1');
+
+buttom1.addEventListener('click',function (){
+    element1.classList.toggle('box-mostrar');
+});
+var buttom2 = document.getElementById('click2');
+var element2 = document.getElementById('added2');
+
+buttom2.addEventListener('click',function (){
+    element2.classList.toggle('box-mostrar');
+});
+
+var buttom3 = document.getElementById('click3');
+var element3 = document.getElementById('added3');
+
+buttom3.addEventListener('click',function (){
+    element3.classList.toggle('box-mostrar');
+});
+
+var buttom4 = document.getElementById('click4');
+var element4 = document.getElementById('added4');
+
+buttom4.addEventListener('click',function (){
+    element4.classList.toggle('box-mostrar');
+});
