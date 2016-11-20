7f9b3d0697af0d38201c295f333ad4dce5f7ffeb
drei109 <drei.rar@gmail.com>
2016-11-20 15:50:51 -0500

    conection changed

--- conexion.php
+++ conexion.php
@@ -1,16 +1,7 @@
 <?php
 function Conectarse()
 {
-//    if (!($link = mysql_connect("br-cdbr-azure-south-b.cloudapp.net", "b9015d4f747069", "57390c3e"))) {
-//        echo "Error conectando a la Base de Datos.";
-//        exit();
-//    }
-//    if (!mysql_select_db("comebien", $link)) {
-//        echo "Error seleccionando Base de Datos.";
-//        exit();
-//    }
-
-    if (!($link = mysql_connect("localhost", "root", ""))) {
+    if (!($link = mysql_connect("br-cdbr-azure-south-b.cloudapp.net", "b9015d4f747069", "57390c3e"))) {
         echo "Error conectando a la Base de Datos.";
         exit();
     }
@@ -18,6 +9,15 @@ function Conectarse()
         echo "Error seleccionando Base de Datos.";
         exit();
     }
+
+    /*if (!($link = mysql_connect("localhost", "root", ""))) {
+        echo "Error conectando a la Base de Datos.";
+        exit();
+    }
+    if (!mysql_select_db("comebien", $link)) {
+        echo "Error seleccionando Base de Datos.";
+        exit();
+    }*/
 
     return $link;
 }
